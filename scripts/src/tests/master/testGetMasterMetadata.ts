f9ed680ecfd4e8e85f602b0f702bb0ef5237338e
Alina-chan <alinabalaur@hotmail.com>
2024-04-17 14:48:02 +0800

    Fix path file for metadata

--- scripts/src/tests/master/testGetMasterMetadata.ts
+++ scripts/src/tests/master/testGetMasterMetadata.ts
@@ -8,13 +8,15 @@ import { join } from "path";
 (async () => {
   try {
     const masterModule = new MasterModule();
-    
+
     // Get last minted Master ID from temp file
-    const masterId = readFileSync(join(__dirname, '..', 'tempMasterId.txt'), { encoding: 'utf-8' });
-    
+    const masterId = readFileSync(
+      join(__dirname, "..", "tempMasterMetadataId.txt"),
+      { encoding: "utf-8" }
+    );
+
     const result = await masterModule.getMasterMetadataById(masterId);
     console.log("Master metadata:", result);
-
   } catch (error) {
     console.error("Failed to find Master:", error);
   }
